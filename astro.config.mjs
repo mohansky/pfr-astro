ec86abdd6d4dd81cbf6a090235bcbdd11fee3e9d
mohansky <38560439+mohansky@users.noreply.github.com>
2025-04-12 19:32:40 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,15 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pawsforrivers.com',
   output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), icon(), react()],
+  integrations: [mdx(), icon(), react(), sitemap()],
   env: {
     schema: {
       RESEND_API_KEY: envField.string({
@@ -21,16 +22,27 @@ export default defineConfig({
         startsWith: "re_",
         optional: false,
       }),
-      // TURSO_DATABASE_URL: envField.string({
-      //   context: "server",
-      //   access: "secret",
-      //   optional: false,
-      // }),
-      // TURSO_AUTH_TOKEN: envField.string({
-      //   context: "server",
-      //   access: "secret",
-      //   optional: false,
-      // }),
+      TURSO_DATABASE_URL: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      TURSO_AUTH_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      // Add the new admin credentials environment variables
+      ADMIN_USERNAME: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      ADMIN_PASSWORD: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
     },
   },
   adapter: netlify(),
