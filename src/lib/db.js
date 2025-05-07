c7c3706447c5b231af133175091ef33edf898ab5
mohansky <38560439+mohansky@users.noreply.github.com>
2025-05-07 14:55:40 +0530

    Delete src directory

--- src/lib/db.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@libsql/client';
-
-export const db = createClient({
-  url: 'libsql://pfr-mohansky.turso.io',
-  authToken: process.env.TURSO_AUTH_TOKEN // Better to use an environment variable
-});
